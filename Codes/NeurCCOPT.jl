fd0874afc73d88bbdd91907d58f9d1d2d8fceea5
Taehyeon Kwon <74909565+TaehyeonKwon@users.noreply.github.com>
2024-05-09 23:44:26 +0900

    Update NeurCCOPT.jl

--- Codes/NeurCCOPT.jl
+++ Codes/NeurCCOPT.jl
@@ -15,7 +15,7 @@ m = 10^2 # number of constraint inside probability
 beta = (1 - alpha)^(1/m)
 lower_bound = 0.0 # lower bound for x
 upper_bound = 10.0 # upper bound for x
-case_type = 0 # case type; 0: independent, 1: dependent
+case_type = 1 # case type; 0: independent, 1: dependent
 
 if case_type == 0
     quantile_value = Distributions.quantile(Chisq(d), beta)
@@ -27,12 +27,12 @@ end
 # 2. hyperparameters
 N = 10^3 # number of scenario
 num_samples_x = 100 # initial generated number of x
-epsilon = 5000 # threshold for embedded nn constraint
+epsilon = 100 # threshold for embedded nn constraint
 seed = 1 # random seed for data generation
 ## 2.1 NN model
 batch_size = 5
 epochs = 30
-learning_rate = 0.001
+learning_rate = 0.01
 ## 2.2 Framework Parameter
 iterations = 1000
 K = 30  # alternating sample size
@@ -75,9 +75,43 @@ function quantile(x, seed)
         sample_xi = generate_sample(seed + i)
         push!(results, cc_g(x, sample_xi))
     end
-    sorted_results = sort(results)
+    sorted_results = sort(results; rev=true)
     index = ceil(Int, alpha * N)
     return sorted_results[index]
+end
+
+
+# Currently working on...
+# Problem: There are big differences between pred value (<10) vs actual value (100<)
+# Solution: Improve model performance
+function plot_quantile_predictions(X_test, Y_test, model, iteration)
+    Y_pred = Float64.([model(x)[1] for x in X_test])
+    Y_test_vec = vec(Float64.(Y_test))
+    min_val = minimum([Y_test_vec; Y_pred]) - 50  
+    max_val = maximum([Y_test_vec; Y_pred]) + 50  
+
+    scatterplot = scatter(Y_test_vec, Y_pred,
+                          label="Predicted vs Actual",
+                          title="Quantile Prediction Accuracy - Iteration $iteration",
+                          xlabel="Actual Quantile",
+                          ylabel="Predicted Quantile",
+                          xlims=(min_val, max_val),
+                          ylims=(min_val, max_val),
+                          legend=:topleft,
+                          size=(600, 600), 
+                          msize=6, 
+                          mcolor=:blue,  
+                          grid=true)  
+
+    plot!(scatterplot, [min_val, max_val], [min_val, max_val],
+          label="Ideal Line y = x", line=(:red, :dash, 2), color=:red)
+
+
+    savepath = "./plots"
+    if !isdir(savepath)
+        mkdir(savepath)
+    end
+    savefig(scatterplot, joinpath(savepath, "iteration_$iteration.png"))
 end
 
 
@@ -91,6 +125,7 @@ end
 X, Y = create_dataset(lower_bound, upper_bound, num_samples_x, seed)
 println("Dataset generated.")
 
+
 train_set_end = floor(Int, length(X) * 0.8)
 
 X_train, X_test = X[1:train_set_end], X[train_set_end+1:end]
@@ -98,12 +133,10 @@ Y_train, Y_test = Y[1:train_set_end], Y[train_set_end+1:end]
 
 X_train = vec(X_train)
 Y_train = vec(Y_train)
-X_test = hcat(X_test...)
-Y_test = hcat(Y_test...)
 
 train_dataset = DataLoader((hcat(X_train...), hcat(Y_train...)), batchsize=batch_size, shuffle=false)
 
-
+# model couldn't predict well.
 function train_NN(train_dataset)
     Random.seed!(123)
     model = Chain(Dense(d, 3, sigmoid), Dense(3, 1))
@@ -114,9 +147,9 @@ function train_NN(train_dataset)
         for batch in train_dataset
             Flux.train!(loss, params, [batch], optimizer)
         end
-        # current_loss = mean([loss(first(batch), last(batch)) for batch in train_dataset])
-        # @info "Epoch: $epoch , Loss:  $current_loss"
-    end 
+        current_loss = mean([loss(first(batch), last(batch)) for batch in train_dataset])
+        @info "Epoch: $epoch , Loss:  $current_loss"
+    end
     return model
 end
 
@@ -137,6 +170,8 @@ function model_validation(model)
 end 
 
 
+#### Case 1: original problem ####
+
 # function solve_norm_opt_probelm(lower_bound, upper_bound, alpha,trained_nn)
 #     opt_model = Model(Ipopt.Optimizer)
 #     set_optimizer_attribute(opt_model, "print_level", 0)
@@ -156,7 +191,7 @@ end
 # end
 
 
-#### binary search case ####
+#### Case 2: obj function included to constraint ####
 
 function solve_norm_opt_probelm(lower_bound, upper_bound, alpha,trained_nn)
     opt_model = Model(Ipopt.Optimizer)
@@ -172,8 +207,12 @@ function solve_norm_opt_probelm(lower_bound, upper_bound, alpha,trained_nn)
     @objective(opt_model, Min,0)
     @operator(opt_model, new_const, d, (x...) -> f(collect(x)))
     @constraint(opt_model, new_const(x...) <= epsilon)
-    @constraint(opt_model, sum(x) >= (2*d))
+    @constraint(opt_model, sum(x) >= (0*d))
     optimize!(opt_model)
+    if !is_solved_and_feasible(opt_model; allow_almost = true)
+        @show(termination_status(opt_model))
+        @warn("Unable to find a feasible and/or optimal solution of the embedded model")
+    end
     return value.(x), objective_value(opt_model)
 end
 
@@ -188,9 +227,10 @@ function iterative_retraining(iterations, K, theta, model)
         @assert model_validation(model)
         x_star_jump,optimal_value = solve_norm_opt_probelm(lower_bound, upper_bound, alpha,model)
         feasi_quantile = quantile(x_star_jump, seed)
+        
+        plot_quantile_predictions(X_train, Y_train, model, iteration)
         if feasi_quantile > 0
             println("Iteration $iteration: Infeasible solution found, x* = $x_star_jump")
-            # println("Iteration $iteration: Infeasible solution found.")
             for k in 1:K
                 bar_x = rand(Uniform(lower_bound, upper_bound), d)
                 x_k = theta*x_star_jump + (1-theta)*bar_x
@@ -202,7 +242,6 @@ function iterative_retraining(iterations, K, theta, model)
             model = train_NN(train_dataset)
         else
             println("Iteration $iteration: Feasible solution found, x* = $x_star_jump. No retraining required.")
-            # println("Optimal value: $(-optimal_value)")
             break
         end
     end
