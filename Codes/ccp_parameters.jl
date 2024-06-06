bdb59a7d34bed83c2c24a70e82f28a7b6057272e
Taehyeon Kwon <74909565+TaehyeonKwon@users.noreply.github.com>
2024-06-06 13:37:29 -0400

    Create ccp_parameters.jl

---
+++ Codes/ccp_parameters.jl
@@ -0,0 +1,36 @@
+module CCPParameters
+
+using Distributions
+
+export setup_parameters
+
+function setup_parameters()
+    params = Dict()
+    
+    # Params for Problem
+    params[:d] = 5
+    params[:alpha] = 0.05
+    params[:m] = 10
+    params[:beta] = (1 - params[:alpha])^(1 / params[:m])
+    params[:lower_bound] = 0.0
+    params[:upper_bound] = 10.0
+    params[:case_type] = 0
+    # params[:quantile_value] = params[:case_type] == 0 ? Distributions.quantile(Chisq(params[:d]), params[:beta]) : Distributions.quantile(Chisq(params[:d]), 1 - params[:alpha])
+    
+    # Params for Framework
+    params[:N] = 1000
+    params[:num_samples_x] = 100
+    params[:epsilon] = 10
+    params[:seed] = 1
+    params[:iterations] = 1000
+    params[:K] = 30
+    params[:theta] = 0.9
+
+    # Params for Neural Net Training
+    params[:batch_size] = 16
+    params[:epochs] = 30
+    params[:learning_rate] = 0.01
+    
+    return params
+end
+end
