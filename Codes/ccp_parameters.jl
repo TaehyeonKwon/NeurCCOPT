4673ecfd15d10f685a9767a2a92e3bbaf0ae9ac6
Taehyeon <kwon981224kwon@gmail.com>
2024-06-11 15:42:53 -0400

    commit today

--- Codes/ccp_parameters.jl
+++ Codes/ccp_parameters.jl
@@ -11,16 +11,15 @@ function setup_parameters(indicator)
         # Params for Problem
         params[:d] = 5
         params[:epsilon] = 0.1 # original confidence level
-        params[:alpha] = 0.11 # confidence level for quantile
-        params[:delta] = 10^(-1) # confidence level for SAA
-        params[:N_SAA] = log(1/params[:delta])/(2*(params[:alpha]-params[:epsilon])^2)*10^2
+        params[:alpha] = 0.2 # confidence level for quantile
+        params[:delta] = 10^(-3) # confidence level for SAA
+        params[:N_SAA] = log(1/params[:delta])/(2*(params[:alpha]-params[:epsilon])^2)
         # params[:N_SAA] = 10^5
         params[:m] = 5
         params[:beta] = (1 - params[:alpha])^(1 / params[:m])
         params[:lower_bound] = 0.0
         params[:upper_bound] = 10.0
         params[:case_type] = 0
-
         # params[:quantile_value] = params[:case_type] == 0 ? Distributions.quantile(Chisq(params[:d]), params[:beta]) : Distributions.quantile(Chisq(params[:d]), 1 - params[:alpha])
     end
 
